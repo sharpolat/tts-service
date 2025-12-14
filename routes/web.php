27de0ea3ea3df9694da5f9cda1c784c76f2bc6c0
Sharpolat <sharpolat@MacBook-Pro-Sharpolat.local>
2025-12-14 19:02:38 +0500

    Move auth.basic to routes instead of controller

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\TtsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [TtsController::class, 'index'])->name('tts.index');
-Route::post('/generate', [TtsController::class, 'generate'])->name('tts.generate');
+Route::get('/', [TtsController::class, 'index'])->name('tts.index')->middleware('auth.basic');
+Route::post('/generate', [TtsController::class, 'generate'])->name('tts.generate')->middleware('auth.basic');
